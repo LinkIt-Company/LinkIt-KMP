18cd8a0fb34d489c0892054850f0ba326b0634f9
Jaesung Lee <viki0114@naver.com>
2026-03-11 17:54:38 +0900

    metro 의존성 추가

--- build-logic/conventions/src/main/kotlin/KmpLibraryConventionPlugin.kt
+++ build-logic/conventions/src/main/kotlin/KmpLibraryConventionPlugin.kt
@@ -6,7 +6,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -15,10 +14,11 @@ class KmpLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
-            
+
             with(pluginManager) {
                 apply("org.jetbrains.kotlin.multiplatform")
                 apply("com.android.library")
+                apply("dev.zacsweers.metro")
             }
 
             extensions.configure<KotlinMultiplatformExtension> {
