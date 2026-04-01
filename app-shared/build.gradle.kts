360c8960f88408087c8080514123de6c8d0e8991
Jaesung Lee <viki0114@naver.com>
2026-04-01 17:14:57 +0900

    모듈간 의존성 방식 수정
    home 모듈을 네비게이션 호스팅 모듈로 설정

--- app-shared/build.gradle.kts
+++ app-shared/build.gradle.kts
@@ -22,10 +22,7 @@ kotlin {
             // Feature modules
             implementation(projects.feature.intro)
             implementation(projects.feature.home)
-            implementation(projects.feature.map)
             implementation(projects.feature.schedule)
-            implementation(projects.feature.storage)
-            implementation(projects.feature.explore)
 
             // Navigation3
             implementation(libs.bundles.jetbrainsNavigation3)
